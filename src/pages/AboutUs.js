5a51139ce211af64d750593974f8646263b96e97
VictorSt3 <vicstoyanov@gmail.com>
2021-03-15 14:44:22 -0400

    ServicesSection

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,8 +1,14 @@
 import React from "react";
 import AboutSection from "../Components/AboutSection";
+import ServicesSection from "../Components/ServicesSection";
 
 const AboutUs = () => {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />;
+      <ServicesSection />
+    </>
+  );
 };
 
 export default AboutUs;
